218ea36837d25d0b2e602ca53b017944072cf27e
Vladimirs <vlarusweb@gmail.com>
2021-07-28 01:00:28 +0300

    Practice, Part 1. Starting to create an application

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 "use strict";
 
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+	count:numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const answerA = prompt('Один из последних просмотренных фильмов?');
+const answerB = prompt('На сколько оцените его?');
+
+const answerC = prompt('Один из последних просмотренных фильмов?');
+const answerD = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[answerA] = answerB;
+personalMovieDB.movies[answerC] = answerD;
